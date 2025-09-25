d489b6a15dcf62242884becb7646a5f4ffb359d7
Ashikuzzaman Roby <mdroby3357@gmail.com>
2025-09-25 17:43:58 +0600

    first code using node js --> require and exports

--- code_01.js
+++ code_01.js
@@ -6,3 +6,8 @@ console.log("Hey, there I am Roby")
 // window object doesn't work here . and npm init , self made script and run with npm like 
 // npm run start 
 // here start is a key . 
+
+
+let math = require("./math.js")
+// console.log(`The value is : ${math(2,3)}`);
+console.log("the value is ", math.add(2,3))
