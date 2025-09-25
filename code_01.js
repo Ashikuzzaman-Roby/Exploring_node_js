84b2cb9704174cf511da5e051106176f7905fb01
Ashikuzzaman Roby <mdroby3357@gmail.com>
2025-09-25 17:01:33 +0600

    set up npm and understanding

---
+++ code_01.js
@@ -0,0 +1,8 @@
+console.log("Hey, there I am js")
+
+console.log("Hey, there I am Roby")
+
+// what I have learned that , 
+// window object doesn't work here . and npm init , self made script and run with npm like 
+// npm run start 
+// here start is a key . 
