a3e6a979718b17c326297d22a160179a5fc573e3
Ashikuzzaman Roby <mdroby3357@gmail.com>
2025-10-04 21:12:48 +0800

    creating rest api

---
+++ Rest_api_practice.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const PORT = 8000;
+
+const app= express();
+app.get("/",(req,res)=>{
+    return res.send(`Hello from home page`)
+});
+
+
+app.get("/about", (req,res)=>{
+    return res.send(`Hey there !! Hello from Roby`);
+    
+});
+
+app.get("/api/users", (req,res)=>{
+    return res.json(users);
+});
+
+app.get("/users" , (req,res)=>{
+    let  html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Rest api creation</title>
+        </head>
+        <style> 
+                body { font-family: Arial, sans-serif; padding: 20px; background-color: #f4f4f9; }
+                ol {  padding: 0,margin-left:30px; }
+                li { background: #fff; margin-bottom: 8px; padding: 10px; border-radius: 6px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
+                h1 { color: #333; }
+                 
+        </style>
+        <body>
+            <h1> All the users are given below </h1>
+            <ol> 
+                 ${
+                     users.map((user) => `<li>${user.first_name}</li>`).join("")
+                 }
+
+
+
+            </ol>
+        </body>
+        </html>  
+    `
+
+    return res.send(html);
+});
+
+
+
+app.get("/user/id" , (req, res)=>{
+    let html = 
+    ` 
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <style> 
+                body { font-family: Arial, sans-serif; padding: 20px; background-color: #f4f4f9; }
+                ol {  padding: 0,margin-left:30px; }
+                li { background: #fff; margin-bottom: 8px; padding: 10px; border-radius: 6px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
+                h1 { color: #333; }
+                 
+        </style>
+
+
+        <body>
+            <h1>User and Id of the users given below</h1>
+            <ol>
+                ${
+                    // ✅ ম্যাপ ফিক্স: user.id এবং user.first_name একসাথে দেখানো হচ্ছে
+                    users.map((user) => `
+                        <li>
+                            ID: <strong>${user.id}</strong> - 
+                            Name: ${user.first_name}
+                        </li>
+                    `).join("")
+                }
+            </ol>
+            
+        </body>
+        </html>
+    `
+    res.send(html);
+})
+
+
+
+app.listen(PORT, ()=>{console.log(`Server is started ...`)});
